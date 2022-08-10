3734154a34f3ba8e7c5a46c5d3ccd88c98249bd8
lucas galarce <galarcelucas@gmail.com>
2022-08-09 23:33:18 -0300

    orderBy param validation

--- src/controller/peopleController.js
+++ src/controller/peopleController.js
@@ -4,6 +4,14 @@ const peopleController = {
   async people(req, res, next) {
     try {
       const { orderBy } = req.query;
+      if (orderBy) {
+        const validOrderBy = ['name', 'height', 'mass'];
+        if (!validOrderBy.includes(orderBy)) {
+          return res.status(400).json({
+            message: 'Order by must be name, height or mass',
+          });
+        }
+      }
       const people = await getPeople(orderBy);
       return res.status(200).json({
         people,
